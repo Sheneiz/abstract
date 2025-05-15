5f994fc3d0f18674c2d2fd774d936eae5ad98c1b
student <a@>
2025-05-15 17:02:07 +0200

    skończ

---
+++ l/src/Main.java
@@ -0,0 +1,4 @@
+public public static void main(String[] args) {
+    DeathCauseStatistic stat = DeathCauseStatistic.fromCsvLine("A04.7          ,758,-,-,-,-,-,1,-,1,3,5,9,12,30,58,64,94,161,192,95,33");
+    System.out.println();
+}
